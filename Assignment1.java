06880ab1da3d8d340ff719601d28bdfd1eba0c2e
bcfeszjing <bcfeszjing@gmail.com>
2024-11-26 17:35:29 +0800

    update Assignment 1

---
+++ Assignment1.java
@@ -0,0 +1,28 @@
+public class Assignment1 {
+
+    // private fields
+
+    private int carYear;
+    private double carPrice;
+    private String carName;
+
+    {/*
+        These are my new changes
+        to the private fields
+    */}
+    public Assignment1(int carYear, double carPrice, String carName){
+        this.carYear = carYear;
+        this.carPrice = carPrice;
+        this.carName = carName;
+    }
+
+    public void displayDetails(){
+        double value = carPrice * carYear;
+        System.out.println(carName + " value is " + value);
+    }
+
+    public static void main(String[] args) {
+        Assignment1 car = new Assignment1(2018,200000,"Tesla");
+        car.displayDetails();
+    }
+}
