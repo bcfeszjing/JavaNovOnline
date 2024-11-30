30876006a2eaa425c1a66b840e82affdf37dae2b
bcfeszjing <bcfeszjing@gmail.com>
2024-11-30 10:58:08 +0800

    no array

--- Assignment2.java
+++ Assignment2.java
@@ -1,20 +1,26 @@
 import java.util.Scanner;
 
-class Car {
-    private String model;
-    private double price;
+class Car { 
+    private String cModel;
+    private String cName;
+    private double cPrice;
 
-    public Car(String model, double price) {
-        this.model = model;
-        this.price = price;
+    public Car(String cModel, String cName, double cPrice) {
+        this.cModel = cModel;
+        this.cName = cName;
+        this.cPrice = cPrice;
     }
 
-    public String getModel() {
-        return model;
+    public String getcModel() {
+        return cModel;
     }
 
-    public double getPrice() {
-        return price;
+    public String getcName() {
+        return cName;
+    }
+
+    public double getcPrice() {
+        return cPrice;
     }
 }
 
@@ -22,30 +28,44 @@ public class Assignment2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Car[] cars = new Car[3];
+        System.out.println("Car 1");
+        System.out.print("Model: ");
+        String model1 = scanner.nextLine();
+        System.out.print("Make: ");
+        String name1 = scanner.nextLine();
+        System.out.print("Price: ");
+        double price1 = scanner.nextDouble();
+        scanner.nextLine(); 
+        Car car1 = new Car(model1, name1, price1);
 
-        for (int i = 0; i < cars.length; i++) {
-            System.out.println("Enter details for car " + (i + 1) + ":");
-            System.out.print("Model: ");
-            String model = scanner.nextLine();
-            System.out.print("Price: ");
-            double price = scanner.nextDouble();
-            scanner.nextLine();
+        System.out.println("Car 2");
+        System.out.print("Model: ");
+        String model2 = scanner.nextLine();
+        System.out.print("Make: ");
+        String name2 = scanner.nextLine();
+        System.out.print("Price: ");
+        double price2 = scanner.nextDouble();
+        scanner.nextLine(); 
+        Car car2 = new Car(model2, name2, price2);
 
-            cars[i] = new Car(model, price);
-        }
+        System.out.println("Car 3");
+        System.out.print("Model: ");
+        String model3 = scanner.nextLine();
+        System.out.print("Make: ");
+        String name3 = scanner.nextLine();
+        System.out.print("Price: ");
+        double price3 = scanner.nextDouble();
+        scanner.nextLine(); 
+        Car car3 = new Car(model3, name3, price3);
 
-        double totalPrice = 0;
-        for (Car car : cars) {
-            totalPrice += car.getPrice();
-        }
+        double totalPrice = car1.getcPrice() + car2.getcPrice() + car3.getcPrice();
         double averagePrice = totalPrice / 3;
 
-        System.out.println("Car Details:");
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Car " + (i + 1) + " - Model: " + cars[i].getModel() + ", Price: " + cars[i].getPrice());
-        }
-        System.out.println("Average Price: " + averagePrice);
+        System.out.println("\nCar Details:");
+        System.out.println("Car 1 - Model: " + car1.getcModel() + ", Name: " + car1.getcName() + ", Price: " + car1.getcPrice());
+        System.out.println("Car 2 - Model: " + car2.getcModel() + ", Name: " + car2.getcName() + ", Price: " + car2.getcPrice());
+        System.out.println("Car 3 - Model: " + car3.getcModel() + ", Name: " + car3.getcName() + ", Price: " + car3.getcPrice());
+        System.out.printf("Average Price: %.2f\n", averagePrice);
 
     }
 }
